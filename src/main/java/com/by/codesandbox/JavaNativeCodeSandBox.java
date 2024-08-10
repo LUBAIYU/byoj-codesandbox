851a9b190363b1bb796d026e42731cb730272634
lzh <1296800094@qq.com>
2024-08-10 22:50:32 +0800

    开放接口，暴露代码沙箱

---
+++ src/main/java/com/by/codesandbox/JavaNativeCodeSandBox.java
@@ -0,0 +1,19 @@
+package com.by.codesandbox;
+
+import com.by.model.ExecuteCodeRequest;
+import com.by.model.ExecuteCodeResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * Java代码沙箱
+ *
+ * @author lzh
+ */
+@Component
+public class JavaNativeCodeSandBox extends JavaCodeSandBoxTemplate {
+
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        return super.executeCode(executeCodeRequest);
+    }
+}
--- src/main/java/com/by/controller/MainController.java
+++ src/main/java/com/by/controller/MainController.java
@@ -1,7 +1,18 @@
 package com.by.controller;
 
+import com.by.codesandbox.JavaNativeCodeSandBox;
+import com.by.model.ExecuteCodeRequest;
+import com.by.model.ExecuteCodeResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 代码沙箱控制器
@@ -10,5 +21,28 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/codesandbox")
+@Slf4j
 public class MainController {
+
+    public static final String AUTH_REQUEST_HEADER = "byoj-auth";
+
+    public static final String AUTH_REQUEST_SECRET = "byoj-secret";
+
+    @Resource
+    private JavaNativeCodeSandBox javaNativeCodeSandBox;
+
+    @PostMapping("/execute")
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request, HttpServletResponse response) {
+        String header = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(header)) {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return null;
+        }
+        if (executeCodeRequest == null) {
+            log.error("executeCodeRequest is null");
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            return null;
+        }
+        return javaNativeCodeSandBox.executeCode(executeCodeRequest);
+    }
 }
